182e3639d3862993a1822d708ef21d7d3c728951
Frederic BECERRIL <fbecerri@e1r4p13.42.fr>
2022-06-20 12:22:59 +0200

    config update

--- configure.sh
+++ configure.sh
@@ -5,4 +5,4 @@ mkdir boost
 ./boost_1_79_0/bootstrap.sh --prefix=/mnt/nfs/homes/fbecerri/ProjetMathInfo/boost --libdir=/mnt/nfs/homes/fbecerri/ProjetMathInfo/boostLib --includedir=/mnt/nfs/homes/fbecerri/ProjetMathInfo/boostInc
 ./boost_1_79_0/b2
 
-LD_LIBRARY_PATH=/mnt/nfs/homes/fbecerri/ProjetMathInfo/boostLib/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/nfs/homes/fbecerri/ProjetMathInfo/boostLib/
